d187c96c502ea9c87d4fcafe1daa1b6863e4e796
mywidget <widget.my.id@gmail.com>
2021-08-06 14:27:32 +0700

    update

--- index.php
+++ index.php
@@ -6,6 +6,5 @@ define('FOLDER', 'class');
 
 include __DIR__ .  DS . FOLDER . DS . 'filter.inc.php';
 include __DIR__ .  DS . FOLDER . DS . 'app_config.php';
-define('BASE_URL', $host);
 include __DIR__ .  DS . FOLDER . DS . 'template.inc.php';
 ?>
