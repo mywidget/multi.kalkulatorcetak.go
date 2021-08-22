12238bdb5dff6b56ee8cb8233b17e14d22156ce2
mywidget <widget.my.id@gmail.com>
2021-08-22 11:32:21 +0700

    Update app_config.php

--- class/app_config.php
+++ class/app_config.php
@@ -9,7 +9,7 @@
 $config = array(
 	"APPS_ID"	=> "673fdc8e-cc8a-45e3-86b6-d3eb16149b45",
 	"APPS_PUBLIC"	=> "cca7231e4e28811e1c2e05b1a17c372120a4f0c5b59fc445bc0a2367388bf74a",
-	"SITE_API"	=> "https://api.kalkulatorcetak.go"
+	"SITE_API"	=> "https://app.kalkulatorcetak.go"
 );
 
-$_URL = $config['SITE_API'].'/apiapp/morea/0/6/'.$config['APPS_ID'].'/'.$config['APPS_PUBLIC']; 
+$_URL = $config['SITE_API'].'/app/load/0/6/'.$config['APPS_ID'].'/'.$config['APPS_PUBLIC']; 
